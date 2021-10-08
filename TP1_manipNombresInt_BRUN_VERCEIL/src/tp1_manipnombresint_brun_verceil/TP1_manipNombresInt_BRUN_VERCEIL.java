157897fae27bd6980f4f9bfe9ef91ecf27be046e
Mathilde <Mathilde@192.168.114.204>
2021-10-08 12:13:46 +0200

    TP1 dépôt 2

--- TP1_manipNombresInt_BRUN_VERCEIL/src/tp1_manipnombresint_brun_verceil/TP1_manipNombresInt_BRUN_VERCEIL.java
+++ TP1_manipNombresInt_BRUN_VERCEIL/src/tp1_manipnombresint_brun_verceil/TP1_manipNombresInt_BRUN_VERCEIL.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+BRUN VERCEIL FGE2 TDC
+TP1 manipNombresInt
+08/10/21
  */
 package tp1_manipnombresint_brun_verceil;
 
@@ -15,7 +15,14 @@ public class TP1_manipNombresInt_BRUN_VERCEIL {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int n1 = 0; // Création des 2 variables recevant les 2 entiers rentrés par l'utilisateur.
+        int n2 = 0;
+        
+        Scanner sc = new Scanner(System.in); //
+        System.out.println("Entrer le nombre :"); // On affiche à l'utilisateur
+        n1 =sc.nextInt(); // On récupère le 1er entier dans notre première variable
+        System.out.println("Entrer le nombre :");
+        n1 =sc.nextInt(); // On récupère le 2ème entier dans notre deuxième variable
     }
     
 }
