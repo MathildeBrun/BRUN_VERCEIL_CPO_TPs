6c777a9777a6e448837ae91fe4efc81f89698a94
Mathilde <Mathilde@192.168.114.204>
2021-10-08 15:15:30 +0200

    TP1 Dépôt 17 / Ex 4

---
+++ TP1_stats_BRUN_VERCEIL/src/tp1_stats_brun_verceil/TP1_stats_BRUN_VERCEIL.java
@@ -0,0 +1,21 @@
+/*
+BRUN VERCEIL FGE2 TDC
+TP1 stats
+08/10/21
+ */
+package tp1_stats_brun_verceil;
+
+/**
+ *
+ * @author Mathilde
+ */
+public class TP1_stats_BRUN_VERCEIL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
