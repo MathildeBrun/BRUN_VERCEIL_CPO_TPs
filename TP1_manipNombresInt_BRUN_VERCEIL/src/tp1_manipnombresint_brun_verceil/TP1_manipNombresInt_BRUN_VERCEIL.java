db539203cb6e3f9d7ff240cf21abc74ad9ff26fd
Mathilde <Mathilde@192.168.114.204>
2021-10-08 14:16:12 +0200

    TP1 Dépôt 11 / Ex 3

--- TP1_manipNombresInt_BRUN_VERCEIL/src/tp1_manipnombresint_brun_verceil/TP1_manipNombresInt_BRUN_VERCEIL.java
+++ TP1_manipNombresInt_BRUN_VERCEIL/src/tp1_manipnombresint_brun_verceil/TP1_manipNombresInt_BRUN_VERCEIL.java
@@ -17,26 +17,28 @@ public class TP1_manipNombresInt_BRUN_VERCEIL {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int nb3 = 0; // Création des 2 variables recevant les 2 entiers rentrés par l'utilisateur.
+        int nb1 = 0; // Création des 2 variables recevant les 2 entiers rentrés par l'utilisateur.
         int nb2 = 0;
         
         Scanner sc = new Scanner(System.in); //
         System.out.println("Entrez un premier nombre :"); // On affiche à l'utilisateur qu'il doit entrer un 1ère nombre.
-        nb3 =sc.nextInt(); // On récupère le 1er entier dans notre première variable
+        nb1 =sc.nextInt(); // On récupère le 1er entier dans notre première variable
         System.out.println("Entrez un deuxième nombre :"); // On affiche à l'utilisateur qu'il doit rentrer un 2ème nombre.
         nb2 =sc.nextInt(); // On récupère le 2ème entier dans notre deuxième variable
-        System.out.println("Nombre 1 : "+ nb3); // On affiche notre premier nombre.
+        System.out.println("Nombre 1 : "+ nb1); // On affiche notre premier nombre.
         System.out.println("Nombre 2 : "+ nb2); // On affiche notre second nombre.
         
-        System.out.println("La somme de vos deux nombre est "+(nb3+nb2)); // Affichage de la somme de nos 2 nombres
-        System.out.println("La différence de vos deux nombre est "+(nb3-nb2)); // Affichage de la différences des 2 nombres
-        System.out.println("Le produit de vos deux nombre est "+(nb3*nb2)); // Affichage du produit de nos 2 nombres
+        System.out.println("La somme de vos deux nombre est "+(nb1+nb2)); // Affichage de la somme de nos 2 nombres
+        System.out.println("La différence de vos deux nombre est "+(nb1-nb2)); // Affichage de la différences des 2 nombres
+        System.out.println("Le produit de vos deux nombre est "+(nb1*nb2)); // Affichage du produit de nos 2 nombres
         
         //Ajoutez ensuite une ligne pour afficher le quotient entier et le reste de la division euclidienne du
         // premier par le second. Pour cela on utilisera deux opérateurs / (division) et % (reste de la division
         // euclidienne). Voir le point cours ci-après.
         
-        System.out.println();
+        System.out.println("Le quotien entier du 1er nombre par le 2ème est : "+(nb1/nb2));
+        System.out.println("Le reste de la division euclidienne du 1er nombre par le 2ème est : "+(nb1%nb2));
+        
         
     }
 }
