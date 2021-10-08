ca32f1fb43e778fd5cd6accce1728aa16d52b31f
Mathilde <Mathilde@192.168.114.204>
2021-10-08 11:38:28 +0200

    TP Test 1 seul dépôt

---
+++ BRUN_VERCEIL_TP0_EX1/src/brun_verceil_tp0_ex1/BRUN_VERCEIL_TP0_EX1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package brun_verceil_tp0_ex1;
+
+/**
+ *
+ * @author Mathilde
+ */
+public class BRUN_VERCEIL_TP0_EX1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
