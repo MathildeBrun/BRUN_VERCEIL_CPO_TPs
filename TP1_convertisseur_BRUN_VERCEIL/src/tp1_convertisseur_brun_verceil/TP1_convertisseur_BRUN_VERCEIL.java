5b270e552e46fa3b1a301b66b4f468624953899e
Mathilde <Mathilde@192.168.114.204>
2021-10-08 15:32:20 +0200

    TP1 Dépôt 18 / Ex 2

--- TP1_convertisseur_BRUN_VERCEIL/src/tp1_convertisseur_brun_verceil/TP1_convertisseur_BRUN_VERCEIL.java
+++ TP1_convertisseur_BRUN_VERCEIL/src/tp1_convertisseur_brun_verceil/TP1_convertisseur_BRUN_VERCEIL.java
@@ -20,33 +20,27 @@ public class TP1_convertisseur_BRUN_VERCEIL {
      */
     
     public static double CelciusVersKelvin (double tCelcius) {
-
-      return tCelcius += 274.15;
+        return tCelcius += 274.15;
         }
     
     public static double KelvinVersCelcius (double tKelvin) {
-
-      return tKelvin -= 274.15;
+        return tKelvin -= 274.15;
         }
     
     public static double FarenheitVersCelcius (double tFarenheit) {
-
-      return tFarenheit = (tFarenheit - 32) / 1.8;
+        return tFarenheit = (tFarenheit - 32) / 1.8;
         }
     
     public static double CelciusVersFarenheit (double tCelcius) {
-
-      return tCelcius = (tCelcius*1.8) + 32;
+        return tCelcius = (tCelcius*1.8) + 32;
         }
     
     public static double KelvinVersFarenheit (double tKelvin) {
-
-      return tKelvin = CelciusVersFarenheit(KelvinVersCelcius(tKelvin));
+        return tKelvin = CelciusVersFarenheit(KelvinVersCelcius(tKelvin));
         }
     
     public static double FarenheitVersKelvin (double tFarenheit) {
-
-      return tFarenheit = FarenheitVersCelcius (CelciusVersKelvin(tFarenheit));
+        return tFarenheit = FarenheitVersCelcius (CelciusVersKelvin(tFarenheit));
         }
     
     public static void main(String[] args) {
@@ -54,15 +48,22 @@ public class TP1_convertisseur_BRUN_VERCEIL {
         
 
                
-        double valeur = 0;
-        
+        double valeur = 0.0;
         Scanner sc = new Scanner(System.in); // commande de demande d'entrée pour l'utilsateurs 
         
-        System.out.println("Bonjour, saisissez une valeur :\n lol");
+        System.out.println("Bonjour, saisissez une valeur : ");
         valeur = sc.nextDouble(); // On récupère la première valeur dans notre première variable
-        
-        
+        System.out.println("Saisissez la conversion que vous souhaiter effectuer :");
+        System.out.println("1) Celcius Vers Kelvin");
+        System.out.println("2) Kelvin Vers Celcius");
+        System.out.println("3) Farenheit Vers Celcius");
+        System.out.println("4) Celcius Vers Farenheit");
+        System.out.println("5) Kelvin Vers Farenheit");
+        System.out.println("6) Farenheit Vers Kelvin");
         
     }
     
 }
+
+//1) De Celcius vers Kelvin 2) De Kelvin vers Celcius … 
+//6) De Farenheit vers Kelvin 2 50 degré Kelvin est égal à -223.15 degrés Celcius
