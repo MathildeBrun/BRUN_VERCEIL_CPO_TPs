860c577229f1a1d3e4e8b147e12566c38a356560
thvel <thvel@LAPTOP-43TL9KUU>
2021-10-08 15:17:26 +0200

    V2 Théo lessInterface

--- TP1_convertisseur_BRUN_VERCEIL/src/tp1_convertisseur_brun_verceil/TP1_convertisseur_BRUN_VERCEIL.java
+++ TP1_convertisseur_BRUN_VERCEIL/src/tp1_convertisseur_brun_verceil/TP1_convertisseur_BRUN_VERCEIL.java
@@ -21,25 +21,48 @@ public class TP1_convertisseur_BRUN_VERCEIL {
     
     public static double CelciusVersKelvin (double tCelcius) {
 
-       
-        double valeur = 0;
-        
-        Scanner sc = new Scanner(System.in); // commande de demande d'entrée pour l'utilsateurs 
-                
-        System.out.println("Entrez une valeur :"); // On affiche à l'utilisateur qu'il doit entrer un 1ère nombre.
-        valeur = sc.nextDouble(); // On récupère la première valeur dans notre première variable
-        
-        System.out.println("valeur en degrés:" + valeur);
-        
-        valeur += 274.15;
-        
+      return tCelcius += 274.15;
+        }
     
-}
+    public static double KelvinVersCelcius (double tKelvin) {
+
+      return tKelvin -= 274.15;
+        }
+    
+    public static double FarenheitVersCelcius (double tFarenheit) {
+
+      return tFarenheit = (tFarenheit - 32) / 1.8;
+        }
+    
+    public static double CelciusVersFarenheit (double tCelcius) {
+
+      return tCelcius = (tCelcius*1.8) + 32;
+        }
+    
+    public static double KelvinVersFarenheit (double tKelvin) {
+
+      return tKelvin = CelciusVersFarenheit(KelvinVersCelcius(tKelvin));
+        }
+    
+    public static double FarenheitVersKelvin (double tFarenheit) {
+
+      return tFarenheit = FarenheitVersCelcius (CelciusVersKelvin(tFarenheit));
+        }
     
     public static void main(String[] args) {
         // TODO code application logic here
         
 
+               
+        double valeur = 0;
+        
+        Scanner sc = new Scanner(System.in); // commande de demande d'entrée pour l'utilsateurs 
+        
+        System.out.println("Bonjour, saisissez une valeur :\n lol");
+        valeur = sc.nextDouble(); // On récupère la première valeur dans notre première variable
+        
+        
+        
     }
     
 }
