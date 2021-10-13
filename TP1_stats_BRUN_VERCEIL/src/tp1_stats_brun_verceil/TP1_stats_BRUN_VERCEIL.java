4ea8a2bce18483e1456cb83117bb68f005084b56
Mathilde <Mathilde@192.168.114.204>
2021-10-13 15:34:14 +0200

    TP1 Dépôt 18 / Ex stats 3e

--- TP1_stats_BRUN_VERCEIL/src/tp1_stats_brun_verceil/TP1_stats_BRUN_VERCEIL.java
+++ TP1_stats_BRUN_VERCEIL/src/tp1_stats_brun_verceil/TP1_stats_BRUN_VERCEIL.java
@@ -5,6 +5,8 @@ TP1 stats
  */
 package tp1_stats_brun_verceil;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Mathilde
@@ -15,7 +17,21 @@ public class TP1_stats_BRUN_VERCEIL {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    // Question 1        
+        int[] face = new int[6]; // réservation de la mémoire
+        face[0]=1; // Mise dans chaque case de sa valeur
+        face[1]=2;
+        face[2]=3;
+        face[3]=4;
+        face[4]=5;
+        face[5]=6;
+    
+    //Question 2
+        int nb = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Veuillez saisir un nombre entier");
+        nb = sc.nextInt();
+        
     }
     
 }
