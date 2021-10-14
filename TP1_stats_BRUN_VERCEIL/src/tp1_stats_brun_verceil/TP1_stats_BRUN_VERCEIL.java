286c4a9594523b70af7b50f9203694855eb12394
Mathilde <Mathilde@192.168.114.204>
2021-10-14 14:09:41 +0200

    TP1 Dépôt 18 / Ex stats 4e

--- TP1_stats_BRUN_VERCEIL/src/tp1_stats_brun_verceil/TP1_stats_BRUN_VERCEIL.java
+++ TP1_stats_BRUN_VERCEIL/src/tp1_stats_brun_verceil/TP1_stats_BRUN_VERCEIL.java
@@ -5,6 +5,7 @@ TP1 stats
  */
 package tp1_stats_brun_verceil;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -19,19 +20,30 @@ public class TP1_stats_BRUN_VERCEIL {
     public static void main(String[] args) {
     // Question 1        
         int[] face = new int[6]; // réservation de la mémoire
-        face[0]=1; // Mise dans chaque case de sa valeur
-        face[1]=2;
-        face[2]=3;
-        face[3]=4;
-        face[4]=5;
-        face[5]=6;
+        face[0]=0; // Mise dans chaque case de sa valeur
+        face[1]=0;
+        face[2]=0;
+        face[3]=0;
+        face[4]=0;
+        face[5]=0;
     
     //Question 2
-        int nb = 0;
+        int m = 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Veuillez saisir un nombre entier");
-        nb = sc.nextInt();
-        
+        m = sc.nextInt();
+     
+    // Question 3
+    int i = 1;
+    Random generateurAleat = new Random();
+
+    for (i=1; i<=m; i++) {
+        int nb = generateurAleat.nextInt(5);
+        face[nb-1]= int(face[nb-1])+1;
+    }
+    
+    System.out.print(face[0]+ ", "+face[1]+", "+face[2]+", "+face[3]+", "+face[4]+", "+face[5]);
+    
     }
     
 }
