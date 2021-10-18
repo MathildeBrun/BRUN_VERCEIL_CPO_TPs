77c914a575b56bb16a59a7fac2d453438b099d62
Mathilde <Mathilde@192.168.114.204>
2021-10-18 10:16:39 +0200

    TP2 Dépôt 01 / Début tp

---
+++ TP2_Bieres_BRUN_VERCEIL/src/tp2_bieres_brun_verceil/TP2_Bieres_BRUN_VERCEIL.java
@@ -0,0 +1,22 @@
+/*
+ BRUN VERCEIL FGE2 TDC
+ TP2 Bieres
+ 08/10/21
+*/
+package tp2_bieres_brun_verceil;
+
+/**
+ *
+ * @author Mathilde
+ */
+public class TP2_Bieres_BRUN_VERCEIL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BouteilleBiere uneBiere = new BouteilleBiere() ;
+                
+    }
+    
+}
