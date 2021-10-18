03b8f54c955e18fb6600add403a16abc4bd9af45
Mathilde <Mathilde@192.168.114.204>
2021-10-18 12:54:33 +0200

    TP2 Dépôt 12 / Ex 2 Question 1 à 5 (on voit le bout)

--- TP2_convertisseurObjet_BRUN_VERCEIL/src/tp2_convertisseurobjet_brun_verceil/TP2_convertisseurObjet_BRUN_VERCEIL.java
+++ TP2_convertisseurObjet_BRUN_VERCEIL/src/tp2_convertisseurobjet_brun_verceil/TP2_convertisseurObjet_BRUN_VERCEIL.java
@@ -3,33 +3,86 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tp2_convertisseurobjet_brun_verceil;
+package tp2_Convertisseurobjet_brun_verceil;
+
+import java.util.Scanner;
+
 
 /**
  *
  * @author Mathilde
  */
-public class TP2_convertisseurObjet_BRUN_VERCEIL {
+public class TP2_ConvertisseurObjet_BRUN_VERCEIL {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         
-        // Création de 2 objet de type Convertisseur et application de diverses conversions
+        // Création de 2 objet de type Convertisseur et application de diverses convUtilisateurersions
         Convertisseur premieresConversions = new Convertisseur() ;
         premieresConversions.CelciusVersFarenheit(50);
         premieresConversions.KelvinVersCelcius(150);
-        premieresConversions.CelciusVersFarenheit(1234);
-
+        premieresConversions.CelciusVersFarenheit(124);
      
         Convertisseur deuxiemesConversions = new Convertisseur() ;
         deuxiemesConversions.KelvinVersFarenheit(1234);
         deuxiemesConversions.CelciusVersKelvin(93746);
+        deuxiemesConversions.KelvinVersFarenheit(0);
         
-        //Affichage du nombre de conversion effectuées par chaque objet.
+        //Affichage du nombre de convUtilisateurersion effectuées par chaque objet.
         System.out.println(premieresConversions);
         System.out.println(deuxiemesConversions);
+        
+        // création du menu
+        Scanner sc = new Scanner(System.in);
+        Convertisseur troisiemeConversions = new Convertisseur();
+        System.out.println("Bonjour, saisissez une valeur : ");
+        double valeur = sc.nextDouble();
+              
+        System.out.println("Saisissez la conversion que vous souhaiter effectuer :"); 
+        System.out.println("1) Celcius Vers Kelvin");
+        System.out.println("2) Kelvin Vers Celcius");
+        System.out.println("3) Farenheit Vers Celcius");
+        System.out.println("4) Celcius Vers Farenheit");
+        System.out.println("5) Kelvin Vers Farenheit");
+        System.out.println("6) Farenheit Vers Kelvin");
+        
+        int convUtilisateur = sc.nextInt();
+        double answer;
+        
+        if (convUtilisateur == 1) {
+            answer = CelciusVersKelvin(valeur);
+            System.out.println(valeur + " degrés Celcius est égal à "+answer+" degrés Kelvin");
+        }
+       
+        if (convUtilisateur == 2) {
+            answer = KelvinVersCelcius(valeur);
+            System.out.println(valeur + " degrés Kelvin est égal à "+answer+" degrés Celcius");
+        }
+        
+        if (convUtilisateur == 3) {
+            answer = FarenheitVersCelcius(valeur);
+            System.out.println(valeur + " degrés Farenheit est égal à "+answer+" degrés Celcius");
+        }
+        
+        if (convUtilisateur == 4) {
+            answer = CelciusVersFarenheit(valeur);
+            System.out.println(valeur + " degrés Celcius est égal à "+answer+" degrés Farenheit");
+        }
+        
+        if (convUtilisateur == 5) {
+            answer = KelvinVersFarenheit(valeur);
+            System.out.println(valeur + " degrés Kelvin est égal à "+answer+" degrés Farenheit");
+        }
+        
+        if (convUtilisateur == 6) {
+            answer = FarenheitVersKelvin(valeur);
+            System.out.println(valeur + " degrés Farenheit est égal à "+answer+" degrés Kelvin");
+        }
+        // Bonus : affichage d'une message d'erreur si le numéro entré ne correspond à aucune convUtilisateurersion.
+        else {
+            System.out.println("Numéro de convUtilisateurersion invalide.");
     }
     
 }
