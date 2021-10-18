8fc26d09cb6df0f108e043f1de0869053b9d81ad
thvel <thvel@LAPTOP-43TL9KUU>
2021-10-18 12:55:06 +0200

    TP2 Partie3

--- TP2_manip_BRUN_VERCEIL/src/tp2_manip_brun_verceil/TP2_manip_BRUN_VERCEIL.java
+++ TP2_manip_BRUN_VERCEIL/src/tp2_manip_brun_verceil/TP2_manip_BRUN_VERCEIL.java
@@ -15,7 +15,43 @@ public class TP2_manip_BRUN_VERCEIL {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        // Initialisation des tartiflette
+        Tartiflette assiette1 = new Tartiflette(500) ; 
+        Tartiflette assiette2 = new Tartiflette(600) ;
+
+        Tartiflette assiette3 = assiette2;
+        
+        // affichage des nb de Calories des tartiflettes
+        System.out.println(assiette1.nbCalories);
+        System.out.println(assiette2.nbCalories);
+        System.out.println(assiette3.nbCalories);
+        
+        
+        // Test d'incrémentation
+        assiette2.nbCalories += 100 ;
+        System.out.println("\nnb de calories de Assiette 3 : " + assiette3.nbCalories) ;
+        
+        assiette3.nbCalories += 100 ;
+        System.out.println("\nnb de calories de Assiette 2 : " + assiette2.nbCalories) ;  // les deux sont liées si on augmente l'une l'autre augmente peut importe l'ordre
+        
+        // Inversion de valeur
+        int stockage = 0;
+                
+        stockage = assiette1.nbCalories; 
+        assiette1.nbCalories = assiette2.nbCalories;
+        assiette2.nbCalories = stockage;
+        
+       // création du tableau référence avec nouvel objet 
+       
+       Moussaka[] service = new Moussaka[10];
+       
+       for (int i=0; i<10; i++) {
+        
+           service[i] = new Moussaka(100);
+        
     }
-    
+    }
 }
+    
+
