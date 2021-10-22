8470a07a39a986a7062df5b996a969af6c619bac
thvel <thvel@LAPTOP-43TL9KUU>
2021-10-22 10:35:02 +0200

    TP2 Vérification du stockage github

---
+++ TP2_manip_BRUN_VERCEIL/src/tp2_manip_brun_verceil/Moussaka.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_brun_verceil;
+
+/**
+ *
+ * @author thvel
+ */
+
+// Initialisation de la class
+public class Moussaka {
+    int nbCalories;   
+   
+// Définition du constructeur
+public Moussaka(int UnNbCalories) {
+
+nbCalories = UnNbCalories;
+
+}
+
+}
+
+
+ 
---
+++ TP2_manip_BRUN_VERCEIL/src/tp2_manip_brun_verceil/Tartiflette.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_brun_verceil;
+
+/**
+ *
+ * @author thvel
+ */
+public class Tartiflette {
+    int nbCalories;
+    
+    
+// Définition du constructeur
+public Tartiflette(int UnNbCalories) {
+
+nbCalories = UnNbCalories;
+
+}    
+}
