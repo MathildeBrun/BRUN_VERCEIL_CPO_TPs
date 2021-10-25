7657af83617f815b4edd9b2b3096dfec48bcfbbf
Mathilde <Mathilde@192.168.114.138>
2021-10-25 16:36:27 +0200

    SP4 / Version 1.0 / classe Partie V1

--- SP4_console_BRUN_VERCEIL/src/sp4_console_brun_verceil/Partie.java
+++ SP4_console_BRUN_VERCEIL/src/sp4_console_brun_verceil/Partie.java
@@ -5,13 +5,43 @@
  */
 package sp4_console_brun_verceil;
 
+import java.util.Random;
+
 /**
  *
  * @author Mathilde
  */
 public class Partie {
+    // Création des attributs
+    Joueur ListeJoueur [] = new Joueur [2];
+    Joueur joueurCourant;
+    Grille grilleJeu = new Grille();
     
+    // Création du constructeur
+    public Partie (Joueur joueur1, Joueur joueur2) {
+        ListeJoueur[0] = joueur1;
+        ListeJoueur[1] = joueur2;
+    }
     
+    // Création des méthodes
+    public void attribuerCouleursAuxJoueurs() {
+        Random generateurAleat = new Random();
+        int num = generateurAleat.nextInt(1);
+        if (num==1){
+            ListeJoueur[1].Couleur = "Rouge";
+        }
+        else {
+            ListeJoueur[1].Couleur = "Jaune";
+        }
+    }
+    
+    public void initialiserPartie() {
+        
+    }
+    
+    public void debuterPartie() {
+        
+    }
 }
 
 
