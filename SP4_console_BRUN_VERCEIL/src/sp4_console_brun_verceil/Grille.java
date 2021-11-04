2ba023997075ed2599029b4dcf5a2c5fa9c08841
Mathilde <Mathilde@192.168.114.138>
2021-11-04 18:47:29 +0100

    SP4 / Version 1.0 / classe Grille + prog Principal Début UI 04/11/21

--- SP4_console_BRUN_VERCEIL/src/sp4_console_brun_verceil/Grille.java
+++ SP4_console_BRUN_VERCEIL/src/sp4_console_brun_verceil/Grille.java
@@ -24,12 +24,11 @@ public class Grille {
     }
 
     
-//ajouterJetonDansColonne(Jeton, int)
-    
+//ajouterJetonDansColonne(Jeton, int)    
     public boolean ajouterJetonDansColonne(Jeton nouveauJetonColonne, int nouvelleColonne) {
         
         // Vérification que la colonne est pleine (on regarde la ligne la plus haute)
-        if (colonneRemplie(uneNouvelleColonne) == true) {   
+        if (colonneRemplie(nouvelleColonne) == true) {   
             return (false);
         }
         // Test de chaque cellule de la collonne tant qu'on en trouve pas une vide
@@ -94,8 +93,7 @@ public class Grille {
     
     
     
-// celluleOccupee : test si la cellule est occupé
-    
+// celluleOccupee : test si la cellule est occupé   
     public boolean celluleOccupee(int li, int co) {
         
         if (CellulesJeu[li][co] == null){
@@ -107,56 +105,59 @@ public class Grille {
     }
     
 // lireCouleurDuJeton
-    
     public String lireCouleurDuJeton(int li, int co) {
         return CellulesJeu[li][co].lireCouleurDuJeton();
     }
     
 // etreGagnantePourJoueur(Joueur)
-    
-    
-    public boolean etreGagnantePourJoueur(Joueur UnJoueur){
-        
-        // Scan vertical    
-        
-        int nbJetonAligne = 0;
-        
+    public boolean etreGagnantePourJoueur(Joueur UnJoueur){     
+    // Scan vertical           
         for (int li = 0; li<6; li++ ) {
-            for (int co = 0; co<7; co++) {
-                
-                
-                
-                if (lireCouleurDuJeton(li,co) == UnJoueur.Couleur ){
-                
-                    nbJetonAligne ++;
-                    
-                    if (nbJetonAligne == 4)
-                        return true;
-                    
-                
-                }
-                else if (l = ) {
-                    
-                }
-                else {
-                    nbJetonAligne = 0; 
-                }
-                  
-            }
-            
+            for (int co = 0; co<4; co++) {                                   
+                if (lireCouleurDuJeton(li,co) == UnJoueur.Couleur & lireCouleurDuJeton(li,co+1) == UnJoueur.Couleur & lireCouleurDuJeton(li,co+2) == UnJoueur.Couleur & lireCouleurDuJeton(li,co+3) == UnJoueur.Couleur ){             
+                    return true;
+                }                
+            }          
         }
-        
+           
+    // Scan horizontal
+        for (int li = 0; li<3; li++ ) {
+            for (int co = 0; co<6; co++) {
+                                    
+                if (lireCouleurDuJeton(li,co) == UnJoueur.Couleur & lireCouleurDuJeton(li+1,co) == UnJoueur.Couleur & lireCouleurDuJeton(li+2,co) == UnJoueur.Couleur & lireCouleurDuJeton(li+3,co) == UnJoueur.Couleur ){                
+                    return true;
+                }                  
+            }            
+        }
+    
+    // Scan diagonale montante
+        for (int li = 0; li<3; li++ ) {
+            for (int co = 0; co<4; co++) {
+                                    
+                if (lireCouleurDuJeton(li,co) == UnJoueur.Couleur & lireCouleurDuJeton(li+1,co+1) == UnJoueur.Couleur & lireCouleurDuJeton(li+2,co+2) == UnJoueur.Couleur & lireCouleurDuJeton(li+3,co+3) == UnJoueur.Couleur ){                
+                    return true;
+                }    
+            }            
+        }
+    
+    // Scan diagonale descendante
+        for (int li = 3; li<6; li++ ) {
+                for (int co = 0; co<4; co++) {
+
+                    if (lireCouleurDuJeton(li,co) == UnJoueur.Couleur & lireCouleurDuJeton(li-1,co+1) == UnJoueur.Couleur & lireCouleurDuJeton(li-2,co+2) == UnJoueur.Couleur & lireCouleurDuJeton(li-3,co+3) == UnJoueur.Couleur ){
+                        return true;
+                    }
+                }
+            } 
+        return false;
     }
     
-    // Scan horizontale 
-    
-    // Scan diagonale droite 
-    
-    // Scan diagonale droite
+// colonneRemplie
     public boolean colonneRemplie(int uneColonne) {
         if (CellulesJeu[5][uneColonne] != null) {   
-            return (true);
+            return true;
+            
         }
+        return false; 
     }
-    
 }
--- SP4_console_BRUN_VERCEIL/src/sp4_console_brun_verceil/SP4_console_BRUN_VERCEIL.java
+++ SP4_console_BRUN_VERCEIL/src/sp4_console_brun_verceil/SP4_console_BRUN_VERCEIL.java
@@ -5,6 +5,8 @@
  */
 package sp4_console_brun_verceil;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Mathilde
@@ -15,8 +17,14 @@ public class SP4_console_BRUN_VERCEIL {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
         
+        System.out.println("Bien le bonjour joueurs !\nVeuillez entrer le nom du premier joueur :");
+        Scanner sc = new Scanner(System.in); 
+        String nomJ1 = sc.next();
+        System.out.println("Bien, merci à vous et bienvenue parmi nous "+nomJ1);
+        System.out.println("A présent, veuillez entrer le nom du premier joueur :");
+        String nomJ2 = sc.next();
+        System.out.println("Bien, merci à vous et bienvenue parmi nous "+nomJ2);
+        System.out.println("Bonne chance "+nomJ1+" et "+nomJ2+"\nQue le meilleur gagne !");
     }
-    
 }
